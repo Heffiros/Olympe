b5bae14413da71fe091517a1bf60d1e1f06cd1b7
Alexandre Levy <vasax.levy@gmail.com>
2025-01-25 22:57:57 +0100

    Clean archi

--- Apollon/src/App.vue
+++ Apollon/src/App.vue
@@ -1,145 +1,71 @@
-<template>
-  <div id="app" class="container">
-    <h1 class="title">Bienvenue dans le projet Apollon</h1>
-    <div class="content">
-      <button @click="startListening" :disabled="isListening">
-        {{ isListening ? 'Écoute en cours...' : 'Démarrer l\'écoute' }}
-      </button>
-      <p v-if="transcription" class="transcription">Transcription : {{ transcription }}</p>
-      <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
-    </div>
-  </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      transcription: '',
-      isListening: false,
-      recognition: null,
-      errorMessage: ''
-    }
-  },
-  methods: {
-    startListening() {
-      if (!('webkitSpeechRecognition' in window || 'SpeechRecognition' in window)) {
-        this.errorMessage = 'Votre navigateur ne supporte pas la reconnaissance vocale'
-        return
-      }
-
-      const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
-      this.recognition = new SpeechRecognition()
-      this.recognition.lang = 'fr-FR'
-      this.recognition.continuous = false
-      this.recognition.interimResults = false
-
-      this.recognition.onstart = () => {
-        this.isListening = true
-        this.errorMessage = ''
-      }
-      this.recognition.onresult = (event) => {
-        this.transcription = event.results[0][0].transcript
-      }
-      this.recognition.onerror = (event) => {
-        this.errorMessage = `Erreur : ${event.error}`
-      }
-      this.recognition.onend = () => {
-        this.sendTranscription()
-        this.isListening = false
-      }
-
-      this.recognition.start()
-    },
-    async sendTranscription() {
-      const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ transcription: this.transcription })
-      };
-
-      try {
-        const response = await fetch('http://localhost:3000/transcription', requestOptions)
-        console.log(response)
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  }
-}
+<script setup>
+import { RouterView } from 'vue-router'
 </script>
 
-<style>
-/* Style général pour le thème sombre */
-body {
-  font-family: Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-  background-color: #2E2E2E;
-  color: #fff;
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
 }
 
-/* Conteneur principal */
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
   text-align: center;
+  margin-top: 2rem;
 }
 
-/* Titre */
-.title {
-  font-size: 3rem;
-  font-weight: bold;
-  color: #fff;
-  margin-bottom: 2rem;
+nav a.router-link-exact-active {
+  color: var(--color-text);
 }
 
-/* Bloc de contenu */
-.content {
-  background: #444;
-  border-radius: 8px;
-  padding: 2rem;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
-  max-width: 400px;
-  width: 100%;
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
 }
 
-/* Bouton */
-button {
-  padding: 0.5rem 1rem;
-  background-color: #007BFF;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 1rem;
-  width: 100%;
-  margin-bottom: 1rem;
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
 }
 
-button:disabled {
-  background-color: #999;
-  cursor: not-allowed;
+nav a:first-of-type {
+  border: 0;
 }
 
-button:hover:not(:disabled) {
-  background-color: #0056b3;
-}
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
 
-/* Texte de transcription */
-.transcription {
-  margin-top: 1rem;
-  font-size: 1.2rem;
-  color: #fff;
-  word-wrap: break-word;
-}
+  .logo {
+    margin: 0 2rem 0 0;
+  }
 
-/* Erreur */
-.error {
-  color: red;
-  margin-top: 1rem;
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
 }
 </style>
